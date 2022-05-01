679643ce0f31e187bc3648a1f43c41cbd8818ef6
12345DavidAvila <94076545+12345DavidAvila@users.noreply.github.com>
2022-05-01 18:43:17 -0500

    cambios pantalla

--- ProyectoRecepcionProductos/Views/Login.xaml.cs
+++ ProyectoRecepcionProductos/Views/Login.xaml.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xamarin.Forms;
 
-namespace ProyectoRecepcionProductos
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProyectoRecepcionProductos.Views
 {
-    public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
     {
-        public MainPage()
+        public Login()
         {
             InitializeComponent();
         }
